d56df5b137e574a7c3af8286fd816e293f1e00f9
kemboi <evanskemb@gmail.com>
2023-02-17 15:01:20 +0300

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char input;
+
+	for (input = 'a'; input <= 'z'; input++)
+	{
+		putchar(input);
+	}
+		putchar('\n');
+	return (0);
+}
