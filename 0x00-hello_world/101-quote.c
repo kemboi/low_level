53f609f90fc8c8e63c115b3cd42cfb948284ef9f
kemboi <evanskemb@gmail.com>
2023-02-17 14:41:49 +0300

    split line for Betty

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <unistd.h>
+#include<string.h>
 /**
  * main - Entry point
  *
@@ -7,6 +8,9 @@
  */
 int main(void)
 {
-	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	int n = 0;
+
+	n = strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", n);
 	return (1);
 }
