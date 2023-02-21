88eb5f5ab4dbe0423ad5c3355266964004c51fec
kemboi <evanskemb@gmail.com>
2023-02-22 02:34:09 +0300

    added

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * jack_bauer - Entry point
+ * Return: none
+ */
+void jack_bauer(void)
+{
+	int hrs, mins;
+
+	for (hrs = 0; hrs < 24; hrs++)
+	{
+		for (mins = 0; mins < 60; mins++)
+		{
+			_putchar((hrs / 10) + '0');
+			_putchar((hrs % 10) + '0');
+			_putchar(':');
+			_putchar((mins / 10) + '0');
+			_putchar((mins % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
