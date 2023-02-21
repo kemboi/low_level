40f0b9edd63e56b23bd582b1680219c7fc1ab951
kemboi <evanskemb@gmail.com>
2023-02-22 01:11:25 +0300

    sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_sign - Entry point
+ * @n: The character to check its sign
+ * Return: When + 1, 0 when 0, and - when <0
+ */
+int print_sign(int n)
+{
+	int result;
+
+	if (n > 0)
+	{
+		result = 1;
+		_putchar('+');
+	}
+	else if (n == 0)
+	{
+		result = 0;
+		_putchar('0');
+	}
+	else
+	{
+		result = -1;
+		_putchar('0');
+	}
+	return (result);
+}
