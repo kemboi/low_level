7fde504621861c16922c0b3cd61fdcb190cfe9af
kemboi <evanskemb@gmail.com>
2023-02-22 03:05:13 +0300

    sum function

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * add - Entry point
+ * @a: first integer
+ * @b: second integer
+ * Description: this function returns the sum of two integers
+ * Return: sum of two integers
+ */
+int add(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+	return (sum);
+}
