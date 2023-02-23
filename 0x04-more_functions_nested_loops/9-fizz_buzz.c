d99210b62d16a92b3251b267d76c9bf9881fba97
kemboi <evanskemb@gmail.com>
2023-02-24 00:43:24 +0300

    added fizzbuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - FizzBuzz
+ * Return: Always return 0
+ */
+int main(void)
+{
+	int i;
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0)
+			printf("Fizz ");
+		else if (i % 5 == 0)
+			printf("Buzz ");
+		else
+			printf("%d ", i);
+	}
+	printf("\n");
+	return (0);
+}
