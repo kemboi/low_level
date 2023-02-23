a4dd5fb90962fe79c50ed221d483f8e37ef684a2
kemboi <evanskemb@gmail.com>
2023-02-24 01:14:16 +0300

    %15 edited space

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -5,20 +5,33 @@
  */
 int main(void)
 {
-	int i;
+	int x;
 
-	for (i = 1; i <= 100; i++)
+	for (x = 1; x <= 100; x++)
 	{
-		if ((i % 5 == 0) && (i % 3 == 0))
+		if ((x % 3 == 0) && (x % 5 == 0))
+		{
 			printf("FizzBuzz");
-		else if (i % 5 == 0)
-			printf("Buzz");
-		else if (i % 3 == 0)
+		}
+		else if (x % 3 == 0)
+		{
 			printf("Fizz");
+		}
+		else if (x % 5 == 0)
+		{
+			printf("Buzz");
+		}
 		else
-			printf("%d", i);
-		printf(" ");
+		{
+			printf("%d", x);
+		}
+
+		if (x != 100)
+		{
+			printf(" ");
+		}
 	}
+
 	printf("\n");
 	return (0);
 }
