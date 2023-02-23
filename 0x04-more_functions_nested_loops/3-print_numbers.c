176358a21cd5cbf85373a77251ca0291230790c1
kemboi <evanskemb@gmail.com>
2023-02-23 22:31:42 +0300

    3-print

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_numbers - from 0..9
+ * Return: none
+ */
+void print_numbers(void)
+{
+	int i;
+
+	i = 48;
+	while (i <= 57)
+	{
+		_putchar(i);
+		i++;
+	}
+	_putchar('\n');
+}
