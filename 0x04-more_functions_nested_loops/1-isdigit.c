3c4716737127f250060994c4760015418ba328b8
kemboi <evanskemb@gmail.com>
2023-02-23 21:48:04 +0300

    part 1

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,12 @@
+/**
+ * _isdigit - checks for digits
+ * @c: integer to check
+ *
+ * Return: 1 if upper and 0 if lower
+ */
+int _isdigit(int c)
+{
+	if (c >= 0 && c <= 9)
+		return (1);
+	return (0);
+}
