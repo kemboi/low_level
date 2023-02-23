e459c6640797b6b39bad158878d7da684cdd54de
kemboi <evanskemb@gmail.com>
2023-02-24 01:29:13 +0300

    added triangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_triangle - print -
+ * @size: number of times dashes will be printed
+ * Return: none
+ */
+void print_triangle(int size)
+{
+	int i, j, k;
+
+	if (size <= 0)
+		_putchar('\n');
+	else
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (j = size - i; j > 1; j--)
+				_putchar(32);
+			for (k = 0; k <= i; k++)
+				_putchar(35);
+		}
+		_putchar('\n');
+	}
+}
