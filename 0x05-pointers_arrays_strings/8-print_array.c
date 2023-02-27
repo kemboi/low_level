5c0ff18e0c7fdd179356e3c06ceb1537b6d06008
kemboi <evanskemb@gmail.com>
2023-02-27 23:32:22 +0300

    part 8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_array - function that returns the length of a string
+ * @a: pointer to char variable
+ * @n: integer variable
+ * Return: nothing
+ */
+void print_array(int *a, int n)
+{
+	int j;
+
+	for (j = 0; j < n; j++)
+	{
+		printf("%d", a[j]);
+		if (j < n-1)
+			printf(", ");
+	}
+	printf("\n");
+}
