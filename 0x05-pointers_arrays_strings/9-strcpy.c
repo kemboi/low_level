a675f7aa2350d23d4265f5ff858cc14824e1f63a
kemboi <evanskemb@gmail.com>
2023-02-27 23:54:56 +0300

    removed counter

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -9,12 +9,10 @@
 char *_strcpy(char *dest, char *src)
 {
 	char *temp = dest;
-	int counter = 0;
 
 	while (*src != '\0')
 	{
 		*dest++ = *src++;
-		counter++;
 	}
 	*dest++ = '\0';
 	return (temp);
