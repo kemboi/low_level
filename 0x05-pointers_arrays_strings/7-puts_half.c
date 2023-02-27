5c3adb20bc188883eeacc45a38d712c0ba08df90
kemboi <evanskemb@gmail.com>
2023-02-27 22:59:53 +0300

    added

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * puts_half - function that returns the length of a string
+ * @str: pointer to char variable
+ * Return: nothing
+ */
+void puts_half(char *str)
+{
+	int len = 0, j;
+
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+	if ((len % 2) == 0)
+	{
+		for (j = len / 2; j < len; j++)
+		{
+			_putchar(str[j]);
+		}
+	}
+	else
+	{
+		for ((j = ((len - 1) / 2)); j < len; j++)
+		{
+			_putchar(str[j]);
+		}
+	}
+	_putchar('\n');
+}
