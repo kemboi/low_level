12add475e8a1bacfb250e956ab784b79da42a7fa
kemboi <evanskemb@gmail.com>
2023-03-15 00:05:47 +0300

    memory leaks

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * free_grid - two grid
+ * @grid:pointer to 2D array
+ * @height:rows of the array
+ * Return: pointer to new string
+ */
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	if (height <= 0)
+		return;
+	for (i = 0 ; i < height; i++)
+	{
+		grid[i] = malloc(0);
+	}
+	grid = malloc(0);
+}
