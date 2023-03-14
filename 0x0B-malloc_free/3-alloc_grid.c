4530f31e652c02ea27b1228051ead568f3d4822f
kemboi <evanskemb@gmail.com>
2023-03-14 23:30:16 +0300

    edited height and width

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -14,20 +14,20 @@ int **alloc_grid(int width, int height)
 
 	if (width == 0 || height == 0 || height < 0 || width < 0)
 		return (NULL);
-	array = malloc(width * sizeof(int *));
+	array = malloc(height * sizeof(int *));
 	if (array == NULL)
 		return (NULL);
-	for (i = 0 ; i < width; i++)
-		array[i] = malloc(height * sizeof(int *));
-	for (i = 0; i < width; i++)
+	for (i = 0 ; i < height; i++)
+		array[i] = malloc(width * sizeof(int *));
+	for (i = 0; i < height; i++)
 	{
-		for (j = 0; j < height; j++)
+		for (j = 0; j < width; j++)
 		{
 			array[i][j] = 0;
 		}
 	}
 	return (array);
-	for (i = 0; i < width; i++)
+	for (i = 0; i < height; i++)
 		free(array[i]);
 	free(array);
 }
