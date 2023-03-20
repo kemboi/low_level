0f65bafa3ebe1afaa62e8bfd7e135c521504aff5
kemboi <evanskemb@gmail.com>
2023-03-20 20:27:23 +0300

    added struct in dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef ST_H
+#define ST_H
+/**
+ * struct dog - my dog structure
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ * Description:a structure about a dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
