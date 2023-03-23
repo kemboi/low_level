e6474e017d024a43c8a9327353d90c334eb48f27
kemboi <evanskemb@gmail.com>
2023-03-23 22:23:13 +0300

    moved the va_end outside the loop

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -21,7 +21,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 			if (*separator && i < (n - 1))
 				printf("%s", separator);
 		}
-		va_end(ap);
 	}
 	printf("\n");
+	va_end(ap);
 }
