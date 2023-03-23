800b3a0df4a98996cc168ec9af293ecbce820400
kemboi <evanskemb@gmail.com>
2023-03-23 22:21:17 +0300

    variadic function with int and char *

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,27 @@
+#include <stdarg.h>
+#include <stdio.h>
+/**
+ * print_numbers - variadic function to sum integers
+ * @separator: string separator
+ * @n: number of arguments
+ * Return: return null
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list ap;
+
+	va_start(ap, n);
+
+	if (n != 0)
+	{
+		for (i = 0; i < n; i++)
+		{
+			printf("%d", va_arg(ap, int));
+			if (*separator && i < (n - 1))
+				printf("%s", separator);
+		}
+		va_end(ap);
+	}
+	printf("\n");
+}
