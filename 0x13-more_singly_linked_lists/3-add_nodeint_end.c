82ed0b4301e153feae5edc8371dc75eb01609dbe
kemboi <evanskemb@gmail.com>
2023-04-03 18:52:56 +0300

    changed the returned value to newNode

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -18,7 +18,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	if (*head == NULL)
 	{
 		*head = newNode;
-		return (*newNode);
+		return (newNode);
 	}
 	while (temp->next != NULL)
 	{
