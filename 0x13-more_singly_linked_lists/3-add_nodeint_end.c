a97a15d3da2fa5315f4430c9d7a3d4cbec68e3cc
kemboi <evanskemb@gmail.com>
2023-04-03 18:59:15 +0300

    changed the returned value to newNode

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -14,6 +14,8 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	newNode = malloc(sizeof(listint_t));
 	if (newNode == NULL)
 		return (NULL);
+	if (!head)
+		return (NULL);
 	temp = *head;
 	if (*head == NULL)
 	{
