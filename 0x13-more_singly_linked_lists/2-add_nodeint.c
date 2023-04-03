06120354939c01929518f4751372cf78c1806f07
kemboi <evanskemb@gmail.com>
2023-04-03 18:39:38 +0300

    edited function to add node at the beginning

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -17,7 +17,7 @@ listint_t *add_nodeint(listint_t **head, const int n)
 	newNode->n = n;
 	newNode->next = *head;
 	*head = newNode;
-	return(*head);
+	return(*newNode);
 	free(newNode);
 	newNode = NULL;
 }
