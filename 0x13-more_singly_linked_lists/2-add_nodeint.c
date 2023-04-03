90d9d45050cc9d2b891d7d96c2fd792d386d13ec
kemboi <evanskemb@gmail.com>
2023-04-03 13:59:41 +0300

    funcition to insert an element at the front of a linked list has been added

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "lists.h"
+#include <stdlib.h>
+/**
+ * add_nodeint - add a new node at the beginning.
+ * @head: head pointer
+ * @n: value of n in the node
+ * Return: returns address of the new element
+ */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *newNode;
+
+	newNode = malloc(sizeof(listint_t));
+	if (newNode == NULL)
+		return (NULL);
+	newNode->n = n;
+	newNode->next = *head;
+	*head = newNode;
+	return (*head);
+}
