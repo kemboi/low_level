114f94942846ab33be807363cc763a89faa2d02d
kemboi <evanskemb@gmail.com>
2023-04-04 14:43:35 +0300

    adding at specific position

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,41 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ * insert_nodeint_at_index - inserts at index
+ * @head: pointer to head
+ * @idx: position to add
+ * @n: data
+ * Return: returns address of new node
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *temp = *head, *newnode, *prev;
+	unsigned int pos = 1;
+
+	while (pos <= idx)
+	{
+		if (temp == NULL)
+		{
+			free(temp);
+			return (NULL);
+		}
+		prev = temp;
+		temp = temp->next;
+		pos++;
+	}
+	newnode = malloc(sizeof(listint_t));
+
+	if (newnode == NULL)
+	{
+		printf("hello");
+		free(temp);
+		return (NULL);
+	}
+	newnode->next = temp;
+	newnode->n = n;
+	temp = temp->next;
+	prev->next = newnode;
+	return (newnode);
+}
+
