67c59ff9088e71ffea1dcfe35c329993b935062b
kemboi <evanskemb@gmail.com>
2023-04-06 18:34:13 +0300

    edited

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include "main.h"
-int checknumber(const char *ch, int count);
+int checknumber(const char *ch);
 /**
  * binary_to_uint - converts from binary to int
  * @b: string pointer
@@ -12,7 +12,6 @@ unsigned int binary_to_uint(const char *b)
 {
 	unsigned int n = 0;
 	int length = strlen(b);
-	int l = length;
 	int power = 0;
 
 	if (b == NULL)
@@ -20,7 +19,7 @@ unsigned int binary_to_uint(const char *b)
 
 	while (length--)
 	{
-		if (checknumber(b, l) == 0)
+		if (checknumber(b) == 0)
 			return (0);
 		n += (*(b + length) == 49) ? (1 << power) : 0;
 		power++;
@@ -30,19 +29,17 @@ unsigned int binary_to_uint(const char *b)
 /**
  * checknumber - checks whether the input string contains only 0 and 1
  * @ch: string pointer to scan
- * @count: length of string
  * Return: 1 if successful or 0 if not.
  */
-int checknumber(const char *ch, int count)
+int checknumber(const char *ch)
 {
 	int i = 0;
 
-	while (count > 0)
+	while (ch[i])
 	{
 		if (ch[i] != 48 && (ch[i] != 49))
 			return (0);
 		i++;
-		count--;
 	}
 	return (1);
 }
