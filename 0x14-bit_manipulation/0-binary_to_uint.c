0cdb342de814f3a685943d1a5364f89fd32f481a
kemboi <evanskemb@gmail.com>
2023-04-06 17:51:20 +0300

    edited

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -14,6 +14,9 @@ unsigned int binary_to_uint(const char *b)
 	int length = strlen(b);
 	int count = length;
 
+	if (b == NULL)
+		return (0);
+
 	while (count--)
 	{
 		if (checknumber(b, length) == 0)
