d0c43be9172d872c14d9758eedd46e21e175db8d
kemboi <evanskemb@gmail.com>
2023-04-08 00:04:33 +0300

    setting a bit function

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * set_bit - sets the bit to 1 given index
+ * @n: pointer to int array
+ * @index: index to check
+ * Return: int value of the bit in the index
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	int mask;
+
+	if (index > 31)
+		return (-1);
+	mask = (1 << index);
+	return (*n |= mask);
+}
