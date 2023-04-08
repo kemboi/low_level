c15984a842a12b927ffe1f931e6ac3d259b06b48
kemboi <evanskemb@gmail.com>
2023-04-08 17:07:28 +0300

    counting number of bits to flip

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * flip_bits - counts number of bits required to flip to change a number
+ * @n: given integer
+ * @m: number to change to 
+ * Return: count
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	int mask;
+
+	mask = (n ^ m);
+	return (count_setbits(mask));
+}
+/**
+ * count_setbits - counts number of set bits
+ * @xor: result of nXORm
+ * Return: integer count
+ */
+unsigned int count_setbits(int xor)
+{
+	int count = 0;
+
+	while (xor > 0)
+	{
+		xor &= (xor - 1);
+		count++;
+	}
+	return count;
+}
