1fc3d7964b4a811f612f0d1398bcdea26c3c0291
kemboi <evanskemb@gmail.com>
2023-04-09 18:21:57 +0300

    removed trailing zero's

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include "main.h"
+void binary_helper(unsigned long int);
 /**
  * print_binary - converts from decimal to binary
  * @n: decimal number
@@ -9,13 +10,25 @@
  */
 void print_binary(unsigned long int n)
 {
-	unsigned int mask = 32768;
-	char c;
-
-	while (mask > 0)
+	if (n == 0)
 	{
-		c = (n & mask) ? '1' : '0';
-		_putchar(c);
-		mask >>= 1;
+		_putchar('0');
+		return;
 	}
+	binary_helper(n);
+}
+/**
+ * binary_helper - shifts right
+ * @n: decimal integer
+ * Return: binary
+ */
+void binary_helper(unsigned long int n)
+{
+	if (n < 1)
+		return;
+	binary_helper(n >> 1);
+	if (n & 1)
+		_putchar('1');
+	else
+		_putchar('0');
 }
