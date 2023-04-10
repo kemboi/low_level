3d3d59db44d15b4cc96498cde9078db13f951485
kemboi <evanskemb@gmail.com>
2023-04-10 17:39:25 +0300

    reading from a file

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include "main.h"
+/**
+ * read_textfile - reads textfile
+ * @filename: file descriptor
+ * @letters: number of letters to read and print
+ * Return: returns the actual number of letters read and printed
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t count;
+	int fp;
+	char *buf;
+
+	if (filename == NULL)
+		return (0);
+	buf = malloc(sizeof(char) * letters);
+	fp = open(filename, O_RDWR, 0);
+
+	if (fp != 3)
+		return (0);
+	count = read(fp, buf, letters);
+	if (write(STDOUT_FILENO, buf, count) != count)
+		return (0);
+	close(fp);
+	free(buf);
+	return (count);
+}
---
+++ 0x15-file_io/main.h
@@ -0,0 +1,4 @@
+#ifndef FILE_H
+#define FILE_H
+ssize_t read_textfile(const char *filename, size_t letters);
+#endif
